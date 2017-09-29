7a1fb1d9ef53726e08de47643012e1d33b7ecaef
Egor Bulgakov <anotheruselessbox@gmail.com>
2017-09-30 02:56:35 +0300

    Меню для всех версий + немного таблетного индекса

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-const button = document.querySelector('.main-nav__toggle'),
-      list = document.querySelector('.main-nav__container');
+var button = document.querySelector('.main-nav__toggle');
+var navBlock = document.querySelector('.main-nav__list--navigation');
+var userBlock = document.querySelector('.main-nav__list--user-block');
 
-button.addEventListener('click', () => {
-  list.classList.toggle('hide');
+button.addEventListener('click', function () {
+  userBlock.classList.toggle('hide');
+  navBlock.classList.toggle('hide');
+
   if (button.textContent === '+') {
     button.textContent = '-';
   }
   else {
     button.textContent = '+';
   }
-});
+})
