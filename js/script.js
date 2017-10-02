3f17d81f41b2d4d1b82763516f2ad2eef12f83d7
Egor Bulgakov <anotheruselessbox@gmail.com>
2017-10-03 01:18:13 +0300

    Реализована логика мобильного меню (с && без) js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 var button = document.querySelector('.main-nav__toggle');
 var navBlock = document.querySelector('.main-nav__list--navigation');
 var userBlock = document.querySelector('.main-nav__list--user-block');
+var image = document.querySelector('.toggler');
 
+/* Если у юзера включен js, нам нужно:
+  1. Скрыть меню
+  2. При взаимодействии тогглить класс -js
+*/
+navBlock.classList.add('hide');  // Скрываем меню
+userBlock.classList.add('hide');
+image.classList.remove('toggler--opened');  // Удаляем положение "открыто" у кнопки
+image.classList.add('toggler--closed');  // И включаем положение "закрыто"
+
+// Тогда при нажатии:
 button.addEventListener('click', function () {
+  // 1. Будет меняться картинка:
+  image.classList.toggle('toggler--opened');
+  image.classList.toggle('toggler--closed');
+  // 2. Будут открываться/скрываться менюшки:
   userBlock.classList.toggle('hide');
   navBlock.classList.toggle('hide');
-
-  if (button.textContent === '+') {
-    button.textContent = '-';
-  }
-  else {
-    button.textContent = '+';
-  }
+  // 3. ...Которые будут спозиционированы абсолютно:
+  userBlock.classList.toggle('main-nav__list--user-block-js');
+  navBlock.classList.toggle('main-nav__list--navigation-js');
 })
